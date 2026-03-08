8c607dc9dd15033d3fc974581445adc485fd94ad
opsty <103743681+opsty@users.noreply.github.com>
2026-03-09 00:18:13 +0800

    Configure GitHub Pages deployment via Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://opsty.github.io',
+	base: 'My_Blog',
 	integrations: [mdx(), sitemap()],
 });
